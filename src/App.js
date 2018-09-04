4a5008029dd108733430faf48a43c5f4bfa8fcd7
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-09-04 12:03:35 -0400

    setting up deploy to github pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Route} from 'react-router-dom'
 import './App.css'
 
-import { Navbar, TitleGenerator, About } from './components'
+import {Navbar, TitleGenerator, About} from './components'
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <Route exact path="/" component={TitleGenerator} />
-                <Route exact path="/about" component={About} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Route exact path="/" component={TitleGenerator} />
+        <Route exact path="/about" component={About} />
+      </div>
+    )
+  }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root')
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>,
+  document.getElementById('root')
 )
 registerServiceWorker()
