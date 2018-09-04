bb2693734b7f334801c0dc1879c96439a1e1f8d5
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-09-04 12:24:17 -0400

    added basename to router for github pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>,
   document.getElementById('root')
